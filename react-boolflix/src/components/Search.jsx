0b4e965e44bc8324c11e18bd990c64bdd50af209
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-06 15:08:55 +0100

    i axios

--- react-boolflix/src/components/Search.jsx
+++ react-boolflix/src/components/Search.jsx
@@ -1,8 +1,23 @@
-export function Search() {
+import { useState, useEffect } from "react"
+
+export function Search( ){
+
+    const [query, setQuery] = useState('')
+
+
+    // useEffect( () => {
+    //     console.log(query)
+    // }, [query])
+
+    const HandleSubmit = (e) => {
+        e.preventDefault()
+        console.log(query)
+    }
+
     return (
         <>
-            <form>
-                <input type="text" />
+            <form onSubmit={e => HandleSubmit(e)}>
+                <input type="text" onChange={ e => setQuery( e.target.value ) } />
                 <button type="submit">Cerca</button>
             </form>
         </>
