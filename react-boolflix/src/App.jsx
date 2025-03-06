c75045b2349c5a789b6a4bf68d4a0d19f481c484
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-06 14:21:52 +0100

    add Header and Main

--- react-boolflix/src/App.jsx
+++ react-boolflix/src/App.jsx
@@ -1,10 +1,13 @@
+import { Header } from "./components/Header"
+import { Main } from "./components/Main"
 
 
 function App() {
   
   return (
     <>
-      
+      <Header />
+      <Main />
     </>
   )
 }
