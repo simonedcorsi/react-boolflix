b472b8c2a1c4b5c95d01ba41a6af0e6817c33e74
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-06 15:48:04 +0100

    .env

--- react-boolflix/src/context/GlobalContext.jsx
+++ react-boolflix/src/context/GlobalContext.jsx
@@ -5,13 +5,25 @@ const GlobalContext = createContext()
 
 const GlobalProvider = ({ children }) => {
 
+    //dati .env
+    const apikey = import.meta.env.VITE_API_KEY
+    const endpoint = import.meta.env.VITE_ENDPOINT
+
+    //data
     const [query, setQuery] = useState('')
+    const [movie, setMovie] = useState([])
+
 
     const HandleSubmit = (e) => {
         e.preventDefault()
-        console.log(query)
+        // console.log(query)
+
+        // https://api.themoviedb.org/3/search/movie?api_key=e99307154c6dfb0b4750f6603256716d&query=ritorno+al+futuro 
+        axios.get(`${endpoint}movie?api_key=${apikey}&query=${query}`)
+        .then( res => setMovie(res.data.results) )
     }
 
+    
     const value = {
         query,
         setQuery,
