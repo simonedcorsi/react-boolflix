934dca5d89fa83f25fd5b0435b87912ae332af36
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-03-06 14:30:32 +0100

    add search

--- react-boolflix/src/components/Header.jsx
+++ react-boolflix/src/components/Header.jsx
@@ -1,5 +1,12 @@
+import { Search } from "./Search"
+
 export function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <h1>Header</h1>
+
+            <Search />
+        </header>
+        
     )
 }
---
+++ react-boolflix/src/components/Search.jsx
@@ -0,0 +1,10 @@
+export function Search() {
+    return (
+        <>
+            <form>
+                <input type="text" />
+                <button type="submit">Cerca</button>
+            </form>
+        </>
+    )
+}
